caa7dbc0c3aa9552c44d4367d5ad80c65898894f
x-wasi <wasi.xwasi.wasix@gmail.com>
2025-10-17 23:26:05 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/projectx';
+const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URL || 'mongodb+srv://onlydevsx_db_user:aN0cWgqkOWo4rhiD@cluster0.jfuzynl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -272,5 +272,6 @@ app.post('/api/sales', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
 
 
